0abe1b32435e1927f8cf739f86223066df0bbdbc
AlexanderNerw <alexnervbiz@gmail.com>
2023-01-18 13:14:58 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,8 @@
+# - *- coding: utf- 8 - *-
+
+from aiogram import Bot, Dispatcher, executor, types
+
+Token = ['5596728441:AAFtwdOdxyOzNjHLbD3q13QBw9z50Q5KuaA']
+
+bot = Bot(token='5596728441:AAFtwdOdxyOzNjHLbD3q13QBw9z50Q5KuaA')
+dp = Dispatcher(bot)
