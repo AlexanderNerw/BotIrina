48bb47ac07787a30466e0d0d5725f68aa12fd9eb
Александр <80785057+AlexanderNerw@users.noreply.github.com>
2023-04-30 21:13:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 from aiogram import Bot, Dispatcher, executor, types
 
-Token = ['5596728441:AAFtwdOdxyOzNjHLbD3q13QBw9z50Q5KuaA']
+Token = ['5644119718:AAG4QKyUab3V7HrjabmcR1r-NtX2IWmOH7Y']
 
-bot = Bot(token='5596728441:AAFtwdOdxyOzNjHLbD3q13QBw9z50Q5KuaA')
+bot = Bot(token='5644119718:AAG4QKyUab3V7HrjabmcR1r-NtX2IWmOH7Y')
 dp = Dispatcher(bot)
